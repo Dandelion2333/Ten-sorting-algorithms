819c0570cb66058741e34ddc2663af4416f54749
Dandelion <494933900@qq.com>
2020-02-08 21:51:45 +0800

    1、学习、实现快速排序 2、学习、实现插入排序

--- Rank.py
+++ Rank.py
@@ -1,7 +1,9 @@
 import random 
 
 minTemp = 0
+quickTemp = 0
 
+########################################################################
 # Get an array whose elements are unordered integers
 def RandomInitList(start, end, length):
     randomList = []
@@ -13,6 +15,7 @@ def RandomInitList(start, end, length):
     
     return randomList
 
+########################################################################
 # Choose a sorting method
 def SelectionSort(SeMatrix):
     exNum = 0
@@ -36,12 +39,75 @@ def SelectionSort(SeMatrix):
 
     return SeMatrix
 
+########################################################################
+# Quickly sorted method calls
+def QuickSortMathod(left, right, QuMatrix):
+    begin = left
+    end = right
+
+    if left > right:
+        return
+
+    while left != right:
+        if QuMatrix[right] <= QuMatrix[begin]:
+            if QuMatrix[left] > QuMatrix[begin]:
+                quickTemp = QuMatrix[left]
+                QuMatrix[left] = QuMatrix[right]
+                QuMatrix[right] = quickTemp
+
+                right = right - 1
+            else:
+                left = left + 1
+        else:
+            right = right - 1
+    
+    quickTemp = QuMatrix[begin]
+    QuMatrix[begin] = QuMatrix[right]
+    QuMatrix[right] = quickTemp
+
+    QuickSortMathod(begin, (left - 1), QuMatrix)
+    QuickSortMathod((right + 1), end, QuMatrix)
+
+# Quickly sorted method
+def QuickSort(QuMatrix):
+    QuickSortMathod(0, (len(QuMatrix) - 1), QuMatrix)
+    
+    print("The result of quick sort is:")
+    print(QuMatrix)
+    return QuMatrix
+
+########################################################################
+def InsertSort(InMatrix):
+    rightCnt = 1
+    while rightCnt < len(InMatrix):
+        if InMatrix[rightCnt] < InMatrix[rightCnt-1]:
+            insertValue = InMatrix[rightCnt]
+            leftCnt = rightCnt - 1
+            while insertValue < InMatrix[leftCnt] and leftCnt > -1:
+                InMatrix[leftCnt+1] = InMatrix[leftCnt] 
+                leftCnt = leftCnt - 1
+            InMatrix[leftCnt+1] = insertValue
+        else:
+            rightCnt = rightCnt + 1
+    
+    print("The result of insert sort is:")
+    print(InMatrix)
+    return InMatrix
+
+
+########################################################################
 if __name__ == "__main__":
     Matrix = []
-    Matrix = RandomInitList(1, 100, 50)
+    Matrix = RandomInitList(1, 200, 100)
     
     # Select Sort
-    SelectionSort(Matrix)
+    #SelectionSort(Matrix)
+
+    # Qucik Sort
+    #QuickSort(Matrix)
+
+    # Insert Sort
+    InsertSort(Matrix)
     
 
 
